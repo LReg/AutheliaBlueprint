10bc70e2b47ec0b4cbc554786332a10b2b40218c
abbing <lreg0@web.de>
2024-10-14 16:22:20 +0200

    0.0.6 [WIP] some fixes

--- docker/scripts/getEnv.sh
+++ docker/scripts/getEnv.sh
@@ -14,7 +14,16 @@ SEARCH_VAR_NAME="$2"
 while IFS= read -r line; do
     if [[ "$line" =~ ^[A-Z_]+=[^\"]+ ]]; then
         VAR_NAME=$(echo "$line" | cut -d'=' -f1)
-
+        if [ "$VAR_NAME" == "$SEARCH_VAR_NAME" ]; then
+          ENV_FILE_VAR_VALUE=$(echo "$line" | cut -d'=' -f2-)
+                  if [ -n "${!VAR_NAME}" ]; then
+                      VAR_VALUE="${!VAR_NAME}"
+                  else
+                      VAR_VALUE="$ENV_FILE_VAR_VALUE"
+                  fi
+                  echo "$VAR_VALUE"
+                  exit 0
+        fi
     fi
 done < "$ENV_FILE"
 exit 1
