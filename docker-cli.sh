fa7d049bf8142642aeaf03386aaeb5ce7121af5d
abbing <lreg0@web.de>
2024-10-14 15:38:08 +0200

    0.0.6 [WIP] some fixes

--- docker-cli.sh
+++ docker-cli.sh
@@ -41,13 +41,13 @@ elif [ "$2" == "up" ]; then
   # up with some extra tags
 
   if [ "$1" == "traefik" ]; then
-    docker compose -f docker/composeFiles/traefik.docker-compose.yml --env-file ./.env "$2" -d
+    docker compose -f docker/composeFiles/traefik.docker-compose.yml --env-file ./.env "$2" -d --force-recreate
   fi
   if [ "$1" == "auth" ]; then
     docker compose -f docker/composeFiles/auth.docker-compose.yml --env-file ./.env "$2" -d --force-recreate --wait --wait-timeout 120
   fi
   if [ "$1" == "frontend" ] || [ "$1" == "backend" ] || [ "$1" == "db" ]; then
-    docker compose -f docker/composeFiles/app.docker-compose.yml --env-file ./.env "$2" "$1" -d
+    docker compose -f docker/composeFiles/app.docker-compose.yml --env-file ./.env "$2" "$1" -d --force-recreate
   fi
 
 fi
