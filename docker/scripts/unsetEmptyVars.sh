1fad8d275940cfd6cf1cfc7b3f346372917d2bcf
abbing <lreg0@web.de>
2024-10-14 16:18:44 +0200

    0.0.6 [WIP] some fixes

--- docker/scripts/unsetEmptyVars.sh
+++ docker/scripts/unsetEmptyVars.sh
@@ -12,12 +12,16 @@ fi
 ENV_FILE="$1"
 LINE_NUMBER=0
 
+echo "Unsetting empty variables in $ENV_FILE"
+
 while IFS= read -r line; do
     LINE_NUMBER=$((LINE_NUMBER+1))
     if [[ "$line" =~ ^[A-Z_]+=[^\"]+ ]]; then
         VAR_NAME=$(echo "$line" | cut -d'=' -f1)
         ENV_FILE_VAR_VALUE=$(echo "$line" | cut -d'=' -f2-)
 
+      echo "Checking $VAR_NAME"
+      echo "Content is $ENV_FILE_VAR_VALUE"
       if [ "$ENV_FILE_VAR_VALUE" == "" ]; then
         unset "$VAR_NAME"
         echo "Unset $VAR_NAME"
