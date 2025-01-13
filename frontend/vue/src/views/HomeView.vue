fb5604517b4f847236a1f85c08ecccd7ab415c7c
abbing <lreg0@web.de>
2025-01-13 15:11:46 +0100

    0.0.7 [WIP] vue

---
+++ frontend/vue/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {environment} from "@/environment/environment.ts";
+import {type Ref, ref} from "vue";
+
+const cu: Ref<any> = ref(null)
+fetch(environment.apiUrl + '/user/current').then(r => {
+  console.log(r)
+  cu.value = r
+})
+
+</script>
+
+<template>
+  HOME
+  {{ cu }}
+</template>
---
+++ frontend/vue/src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+const handleLogin = () => {
+
+}
+</script>
+
+<template>
+  <button @click="handleLogin">Login</button>
+</template>
+
+<style scoped>
+
+</style>
